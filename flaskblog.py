d72977aca35d0afaa7eca0900a7321b3308c8459
eddie-coding <adityabahuguna007@gmail.com>
2020-05-11 23:06:52 +0530

    --

--- flaskblog.py
+++ flaskblog.py
@@ -1,18 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Aditya',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 11, 2020'
+    },
+    {
+        'author': 'Eddie',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 12, 2020'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    return render_template('about.html')
-
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
